649aaee4cf983e029aa1abb22275b33234e7db79
jeffgribeiro <jeffgribeiro@gmail.com>
2021-03-26 22:22:20 -0300

    Inserindo título label

--- App/classes/ModView.php
+++ App/classes/ModView.php
@@ -8,10 +8,11 @@ class ModView
 {
     public static function index()
     {
+        echo '<h2>Sequence of multiples of 3, 5 and 15</h2><br>';
         $module = new Mod;
 
         for ($i = 1; $i <= 100; $i++) {
-            echo '<br> ' . $module->module($module->module($module->module($i, 15, 'ThreeAndFive'), 3, 'Three'), 5, 'Five');
+            echo ' ' . $module->module($module->module($module->module($i, 15, 'ThreeAndFive'), 3, 'Three'), 5, 'Five');
         }
     }
 }
